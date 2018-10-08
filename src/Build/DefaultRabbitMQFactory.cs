9da009f8d3818968527b9b6017ab2d8e20f66556
EgorNuzhnykh <nuzhnykhev@gmail.com>
2018-10-08 14:35:31 +0300

    Fixed documentation for package

--- src/Build/DefaultRabbitMQFactory.cs
+++ src/Build/DefaultRabbitMQFactory.cs
@@ -7,7 +7,7 @@ namespace PipServices.RabbitMQ.Build
     /// <summary>
     /// Creates RabbitMQMessageQueue components by their descriptors.
     /// </summary>
-    /// See <see cref="RabbitMQMessageQueue"/>
+    /// See <a href="https://rawgit.com/pip-services-dotnet/pip-services-rabbitmq-dotnet/master/doc/api/class_pip_services_1_1_rabbit_m_q_1_1_queues_1_1_rabbit_m_q_message_queue.html">RabbitMQMessageQueue</a>
     public class DefaultRabbitMQFactory: Factory
     {
         public static Descriptor Descriptor = new Descriptor("pip-services", "factory", "rabbitmq", "default", "1.0");
--- src/Queues/RabbitMQMessageQueue.cs
+++ src/Queues/RabbitMQMessageQueue.cs
@@ -21,22 +21,24 @@ namespace PipServices.RabbitMQ.Queues
     /// 
     /// ### Configuration parameters ###
     /// 
-    /// topic:                         name of MQTT topic to subscribe
+    /// - topic:                         name of MQTT topic to subscribe
+    /// 
     /// connection(s):
-    /// discovery_key:               (optional) a key to retrieve the connection from IDiscovery
-    /// host:                        host name or IP address
-    /// port:                        port number
-    /// uri:                         resource URI or connection string with all parameters in it
+    /// - discovery_key:               (optional) a key to retrieve the connection from <a href="https://rawgit.com/pip-services-dotnet/pip-services-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a>
+    /// - host:                        host name or IP address
+    /// - port:                        port number
+    /// - uri:                         resource URI or connection string with all parameters in it
+    /// 
     /// credential(s):
-    /// store_key:                   (optional) a key to retrieve the credentials from ICredentialStore
-    /// username:                    user name
-    /// password:                    user password
+    /// - store_key:                   (optional) a key to retrieve the credentials from <a href="https://rawgit.com/pip-services-dotnet/pip-services-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_auth_1_1_i_credential_store.html">ICredentialStore</a>
+    /// - username:                    user name
+    /// - password:                    user password
     /// 
     /// ### References ###
     /// 
-    /// - *:logger:*:*:1.0             (optional) ILogger components to pass log messages
-    /// - *:counters:*:*:1.0           (optional) ICounters components to pass collected measurements
-    /// - *:discovery:*:*:1.0          (optional) IDiscovery services to resolve connections
+    /// - *:logger:*:*:1.0             (optional) <a href="https://rawgit.com/pip-services-dotnet/pip-services-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_log_1_1_i_logger.html">ILogger</a> components to pass log messages
+    /// - *:counters:*:*:1.0           (optional) <a href="https://rawgit.com/pip-services-dotnet/pip-services-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_count_1_1_i_counters.html">ICounters</a> components to pass collected measurements
+    /// - *:discovery:*:*:1.0          (optional) <a href="https://rawgit.com/pip-services-dotnet/pip-services-components-dotnet/master/doc/api/interface_pip_services_1_1_components_1_1_connect_1_1_i_discovery.html">IDiscovery</a> services to resolve connections
     /// - *:credential-store:*:*:1.0   (optional) Credential stores to resolve credentials
     /// </summary>
     /// <example>
@@ -54,7 +56,6 @@ namespace PipServices.RabbitMQ.Queues
     /// queue.Complete("123", message);
     /// </code>
     /// </example>
-    /// See <see cref="MessageQueue"/>, <see cref="MessagingCapabilities"/>
     public class RabbitMQMessageQueue : MessageQueue
     {
         //private long DefaultVisibilityTimeout = 60000;
