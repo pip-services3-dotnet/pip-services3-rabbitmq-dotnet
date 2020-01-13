4c663cf343acf65765e7a7cbeb0607e29e98d53a
Danyil Tretiakov <daniil.tr@hotmail.com>
2020-01-13 21:24:27 +0200

    Added 'pip-services' descriptors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,15 @@
 # RabbitMQ specific components for Pip.Services in .NET Changelog
 
-## <a name="3.0.0"></a> 3.0.0 (2018-08-07)
+## <a name="3.0.0-3.0.4"></a> 3.0.0-3.0.4 (2020-01-13)
 
 Initial public release for Pip.Services 3.0
 
 ### Features
 * **build** RabbitMQ queues factory
 * **messaging** RabbitMQ queues
+
+### Breaking Changes
+* Added 'pip-services' descriptors
 
 ### Bug Fixes
 No fixes in this version
--- src/Build/DefaultRabbitMQFactory.cs
+++ src/Build/DefaultRabbitMQFactory.cs
@@ -10,9 +10,12 @@ namespace PipServices3.RabbitMQ.Build
     /// See <a href="https://rawgit.com/pip-services3-dotnet/pip-services3-rabbitmq-dotnet/master/doc/api/class_pip_services_1_1_rabbit_m_q_1_1_queues_1_1_rabbit_m_q_message_queue.html">RabbitMQMessageQueue</a>
     public class DefaultRabbitMQFactory: Factory
     {
-        public static Descriptor Descriptor = new Descriptor("pip-services3", "factory", "rabbitmq", "default", "1.0");
-        public static Descriptor RabbitMQMessageQueueFactoryDescriptor = new Descriptor("pip-services3", "factory", "message-queue", "rabbitmq", "1.0");
-        public static Descriptor RabbitMQMessageQueueDescriptor = new Descriptor("pip-services3", "message-queue", "rabbitmq", "*", "1.0");
+        public static Descriptor Descriptor = new Descriptor("pip-services", "factory", "rabbitmq", "default", "1.0");
+        public static Descriptor Descriptor3 = new Descriptor("pip-services3", "factory", "rabbitmq", "default", "1.0");
+        public static Descriptor RabbitMQMessageQueueFactoryDescriptor = new Descriptor("pip-services", "factory", "message-queue", "rabbitmq", "1.0");
+        public static Descriptor RabbitMQMessageQueueFactory3Descriptor = new Descriptor("pip-services3", "factory", "message-queue", "rabbitmq", "1.0");
+        public static Descriptor RabbitMQMessageQueueDescriptor = new Descriptor("pip-services", "message-queue", "rabbitmq", "*", "1.0");
+        public static Descriptor RabbitMQMessageQueue3Descriptor = new Descriptor("pip-services3", "message-queue", "rabbitmq", "*", "1.0");
 
         /// <summary>
         /// Create a new instance of the factory.
@@ -20,7 +23,9 @@ namespace PipServices3.RabbitMQ.Build
         public DefaultRabbitMQFactory()
         {
             RegisterAsType(RabbitMQMessageQueueFactoryDescriptor, typeof(RabbitMQMessageQueueFactory));
+            RegisterAsType(RabbitMQMessageQueueFactory3Descriptor, typeof(RabbitMQMessageQueueFactory));
             RegisterAsType(RabbitMQMessageQueueDescriptor, typeof(RabbitMQMessageQueue));
+            RegisterAsType(RabbitMQMessageQueue3Descriptor, typeof(RabbitMQMessageQueue));
         }
     }
 }
