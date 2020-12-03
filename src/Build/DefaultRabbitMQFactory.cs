215ff0267094e6a1a109a5b2b33870f0d546a199
banalna <judas.priest999@gmail.com>
2020-12-03 14:40:25 +0300

    fix links in docs

--- src/Build/DefaultRabbitMQFactory.cs
+++ src/Build/DefaultRabbitMQFactory.cs
@@ -7,7 +7,7 @@ namespace PipServices3.RabbitMQ.Build
     /// <summary>
     /// Creates RabbitMQMessageQueue components by their descriptors.
     /// </summary>
-    /// See <a href="https://rawgit.com/pip-services3-dotnet/pip-services3-rabbitmq-dotnet/master/doc/api/class_pip_services_1_1_rabbit_m_q_1_1_queues_1_1_rabbit_m_q_message_queue.html">RabbitMQMessageQueue</a>
+    /// See <a href="https://pip-services3-dotnet.github.io/pip-services3-rabbitmq-dotnet/class_pip_services_1_1_rabbit_m_q_1_1_queues_1_1_rabbit_m_q_message_queue.html">RabbitMQMessageQueue</a>
     public class DefaultRabbitMQFactory: Factory
     {
         public static Descriptor Descriptor = new Descriptor("pip-services", "factory", "rabbitmq", "default", "1.0");
