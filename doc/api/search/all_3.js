ced306d30b37df2b55fefd9846853eff421bcb19
banalna <judas.priest999@gmail.com>
2020-12-03 14:44:14 +0300

    delete component.conf.ps1 and doc folder

--- doc/api/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['endlisten',['EndListen',['../class_pip_services_1_1_rabbit_m_q_1_1_queues_1_1_rabbit_m_q_message_queue.html#a32efd04355b31a00debe6e10dbc12e62',1,'PipServices3::RabbitMQ::Queues::RabbitMQMessageQueue']]]
-];
--- doc/api/search/functions_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['movetodeadletterasync',['MoveToDeadLetterAsync',['../class_pip_services_1_1_rabbit_m_q_1_1_queues_1_1_rabbit_m_q_message_queue.html#a56442bc60f22ff3a2d42e2242ece7de4',1,'PipServices3::RabbitMQ::Queues::RabbitMQMessageQueue']]]
-];
